5f7e6cf15443559357602acb92e5f3900998dd8c
behzodjon <behzodjontuit1998@gmail.com>
2023-09-17 20:03:38 +0500

    fix: bugs

--- src/components/SelectLang.vue
+++ src/components/SelectLang.vue
@@ -1,39 +1,41 @@
 <script setup>
-
-import {onMounted, ref, watch} from "vue";
-import {useI18n} from "vue-i18n";
+import { onMounted, ref, watch } from "vue";
+import { useI18n } from "vue-i18n";
 
 const languages = [
-  {name: 'English', code: 'en'},
-  {name: `O'zbek`, code: 'uz'},
-  {name: 'Ўзбекча', code: 'uz_cryl'},
-  {name: 'Русский', code: 'ru'},
-  {name: 'العربية', code: 'ar'}
-]
+  { name: "English", code: "en" },
+  { name: `O'zbek`, code: "uz" },
+  { name: "Ўзбекча", code: "uz_cryl" },
+  { name: "Русский", code: "ru" },
+  { name: "العربية", code: "ar" },
+];
 
 const { locale } = useI18n();
 watch(locale, (newVal) => {
-  localStorage.setItem('locale', newVal)
-  const html = document.getElementsByTagName('html')[0]
-  html.setAttribute('dir', newVal === 'ar' ? 'rtl' : 'ltr')
-})
+  window.location.reload();
+
+  localStorage.setItem("locale", newVal);
+  const html = document.getElementsByTagName("html")[0];
+  html.setAttribute("dir", newVal === "ar" ? "rtl" : "ltr");
+});
 
 onMounted(() => {
- const html = document.getElementsByTagName('html')[0]
-  const locale = localStorage.getItem('locale')
+  const html = document.getElementsByTagName("html")[0];
+  const locale = localStorage.getItem("locale");
   if (locale) {
-    html.setAttribute('dir', locale === 'ar' ? 'rtl' : 'ltr')
+    html.setAttribute("dir", locale === "ar" ? "rtl" : "ltr");
   } else {
-    html.setAttribute('dir', 'ltr')
+    html.setAttribute("dir", "ltr");
   }
-})
-
+});
 </script>
 
 <template>
   <div class="select-lang">
     <select v-model="$i18n.locale">
-      <option v-for="lang in languages" :key="lang.code" :value="lang.code">{{ lang.name }}</option>
+      <option v-for="lang in languages" :key="lang.code" :value="lang.code">
+        {{ lang.name }}
+      </option>
     </select>
   </div>
 </template>
@@ -41,6 +43,10 @@ onMounted(() => {
 <style scoped>
 .select-lang {
   z-index: 100;
+}
+
+html[dir="rtl"] .select-lang {
+  margin-left: 20px;
 }
 
 .select-lang select {
@@ -55,7 +61,6 @@ onMounted(() => {
   background-color: #fff;
   transition: all 0.3s ease;
 }
-
 
 .select-lang select option {
   font-size: 16px;
